93235cb245998973e3c128cf62b529a2eb18048c
Nishand-RM <nishandrm9@gmail.com>
2024-08-12 12:18:35 +0530

    useReduce hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,68 @@
-
-
-import { Button } from 'bootstrap';
 import React from 'react'
+import { useReducer } from 'react';
 import { useRef } from 'react';
+
+
+const initial = 0;
+
+
+function reducer(state,action){
+//reducer function is called whwn dispatch is called
+//action will point to the object that passed in the dispatch
+//state argument will be the current state of the component
+//expected from the reducer function to return a new state
+
+switch (action.type)
+  {
+    case "INCREMENT":
+      return state+1;
+
+      case "DECREMENT":
+        return state-1;
+
+      case "RESET":
+      return initial;
+
+      
+
+  }
+}
+
+
+
 
 const App = () => {
 
-  const countref = useRef(0);
+const [count,dispatch] = useReducer(reducer,initial) ; 
   
-  const inc = ()=>{
-    //move the focus to input box
 
-    countref.current += 1;
-    console.log(countref.current);
+const inc = ()=>{
+    dispatch({type: "INCREMENT"})
   }
+
+
+  const dec = ()=>{
+    dispatch({type: "DECREMENT"})
+  }
+
+
+
+  const rst = ()=>{
+    dispatch({type: "RESET"})
+  }
+
+ 
   
   return (
     <div>
-      <h1>Count:{countref.current}</h1>
+      <h1>Count:{count}</h1>
       
       
       <button  onClick={inc}>INC</button>     
+      <button  onClick={dec}>DEC</button>     
+      <button  onClick={rst}>RST</button>    
+ 
+
       </div>
   )
 }
